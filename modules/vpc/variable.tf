a0d98af5402a88f48a559758ebceb72a3e635987
PradeepKaushik <45539612+pradepkaushik@users.noreply.github.com>
2026-07-11 15:12:50 +0530

    Update variable.tf

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -1,1 +1,23 @@
+#####################################
+# VPC Variables
+#####################################
 
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_1" {
+  description = "CIDR block for Public Subnet 1"
+  type        = string
+}
+
+variable "public_subnet_2" {
+  description = "CIDR block for Public Subnet 2"
+  type        = string
+}
+
+variable "private_subnet_1" {
+  description = "CIDR block for Private Subnet 1"
+  type        = string
+}
