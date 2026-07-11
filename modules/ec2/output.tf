69bcb8b0f3143696c52246fd245b9ac690eb6b11
PradeepKaushik <45539612+pradepkaushik@users.noreply.github.com>
2026-07-11 15:28:44 +0530

    Update output.tf

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,1 +1,33 @@
+#####################################
+# EC2 Outputs
+#####################################
 
+output "instance_id" {
+  description = "EC2 Instance ID"
+  value       = aws_instance.ec2.id
+}
+
+output "instance_arn" {
+  description = "EC2 Instance ARN"
+  value       = aws_instance.ec2.arn
+}
+
+output "public_ip" {
+  description = "EC2 Public IP"
+  value       = aws_instance.ec2.public_ip
+}
+
+output "private_ip" {
+  description = "EC2 Private IP"
+  value       = aws_instance.ec2.private_ip
+}
+
+output "public_dns" {
+  description = "EC2 Public DNS"
+  value       = aws_instance.ec2.public_dns
+}
+
+output "security_group_id" {
+  description = "Security Group ID"
+  value       = aws_security_group.ec2_sg.id
+}
