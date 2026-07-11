bbe25d1d8008664975d80e260de7f365a4f958dd
PradeepKaushik <45539612+pradepkaushik@users.noreply.github.com>
2026-07-11 15:14:39 +0530

    Update output.tf

--- modules/s3/output.tf
+++ modules/s3/output.tf
@@ -1,1 +1,28 @@
+#####################################
+# S3 Outputs
+#####################################
 
+output "bucket_name" {
+  description = "Name of the S3 bucket"
+  value       = aws_s3_bucket.bucket.bucket
+}
+
+output "bucket_id" {
+  description = "ID of the S3 bucket"
+  value       = aws_s3_bucket.bucket.id
+}
+
+output "bucket_arn" {
+  description = "ARN of the S3 bucket"
+  value       = aws_s3_bucket.bucket.arn
+}
+
+output "bucket_domain_name" {
+  description = "Bucket Domain Name"
+  value       = aws_s3_bucket.bucket.bucket_domain_name
+}
+
+output "bucket_regional_domain_name" {
+  description = "Regional Domain Name"
+  value       = aws_s3_bucket.bucket.bucket_regional_domain_name
+}
