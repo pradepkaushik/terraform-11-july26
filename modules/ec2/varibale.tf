6116c77d9be987245272ef1b9ee5229641073d90
PradeepKaushik <45539612+pradepkaushik@users.noreply.github.com>
2026-07-11 15:28:18 +0530

    Update varibale.tf

--- modules/ec2/varibale.tf
+++ modules/ec2/varibale.tf
@@ -1,1 +1,28 @@
+#####################################
+# EC2 Variables
+#####################################
 
+variable "instance_name" {
+  description = "Name of the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "AWS Key Pair Name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the EC2 instance will be created"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where the EC2 instance will be launched"
+  type        = string
+}
