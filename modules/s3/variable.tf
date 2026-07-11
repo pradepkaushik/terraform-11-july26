74189ac5f69e5ab13989e0480fd2e2a707a856e9
PradeepKaushik <45539612+pradepkaushik@users.noreply.github.com>
2026-07-11 15:14:14 +0530

    Update variable.tf

--- modules/s3/variable.tf
+++ modules/s3/variable.tf
@@ -1,1 +1,8 @@
+#####################################
+# S3 Variables
+#####################################
 
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
